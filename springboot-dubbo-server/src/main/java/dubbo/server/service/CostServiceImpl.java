dc0e784c422aa38158afe1f71cf2214444a1d9f8
MrCuriositer <zhaoyy330901@163.com>
2019-05-06 10:42:52 +0800

    #增加注释

--- springboot-dubbo-server/src/main/java/dubbo/server/service/CostServiceImpl.java
+++ springboot-dubbo-server/src/main/java/dubbo/server/service/CostServiceImpl.java
@@ -3,13 +3,19 @@ package dubbo.server.service;
 import com.alibaba.dubbo.config.annotation.Service;
 import dubbo.api.CostService;
 
+/**
+ * dubbo服务接口 CostService
+ * CostServiceImpl 服务的具体实现
+ */
 @Service
 public class CostServiceImpl implements CostService {
+
     private final Integer totalCost = 1000;
 
     @Override
     public Integer add(int cost) {
         System.out.println("调用costservice的实现类...");
+        System.out.println("dubbo服务实现类执行");
         return cost+totalCost;
     }
 }
